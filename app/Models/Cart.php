382238499d60b5619da2d9b21381b0c09533365a
Prabhu <vprabhu1771@gmail.com>
2024-12-02 15:28:40 +0530

    Cart migration and model
    php artisan make:model Cart -m

---
+++ app/Models/Cart.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    use HasFactory;
+    
+    protected $fillable =[
+        'customer_id',
+        'product_id',
+        'qty',
+    ];
+
+    public function customer()
+    {
+        return $this->belongsTo(User::class, 'customer_id');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class,'product_id');
+    }
+
+    public function totalPrice()
+    {
+        return $this->qty = $this->product->price;
+    }
+
+    // public function grandTotal($customerId)
+    // {
+    //    $cartItems = Cart::where('customer_id',$customerId)->get();
+       
+    //    $total = $cartItemms->sum(function($item){
+    //     return $item->totalPrice();
+    //    });
+
+    //    return $total;
+    // }
+    
+    public static function grandTotal($customerId)
+    {
+        $cartItems = Cart::where('customer_id', $customerId)->get();
+        $total = $cartItems->sum(function ($item) {
+            return $item->totalPrice();
+        });
+
+        return $total;
+    }
+}
