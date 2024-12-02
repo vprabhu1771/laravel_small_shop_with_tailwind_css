24e2435d45dd5e7f70f04453bc96eb3304b20bf6
Prabhu <vprabhu1771@gmail.com>
2024-12-02 12:07:35 +0530

    BrandSeeder added to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,8 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call([
-            CategorySeeder::class
+            CategorySeeder::class,
+            BrandSeeder::class
         ]);
     }
 }
