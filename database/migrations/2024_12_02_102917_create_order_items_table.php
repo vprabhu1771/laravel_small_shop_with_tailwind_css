cb09895ca4f18808626291333cf65ef2d25e0002
Prabhu <vprabhu1771@gmail.com>
2024-12-02 16:03:58 +0530

    OrderItem migration and model
    php artisan make:model OrderItem -m

---
+++ database/migrations/2024_12_02_102917_create_order_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('product_id');
+            
+            $table->integer('qty')->default(1);
+            
+            $table->decimal('unit_price', 10, 2); // Price of the individual item
+            $table->decimal('sub_total', 10, 2); // Price * quantity
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_items');
+    }
+};
