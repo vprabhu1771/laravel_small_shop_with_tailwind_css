7e9e79376324665879fa9ab7956f247960fe7d40
Prabhu <vprabhu1771@gmail.com>
2024-12-02 13:16:55 +0530

    ProductSeeder added to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,7 +25,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             CategorySeeder::class,
-            BrandSeeder::class
+            BrandSeeder::class,
+            ProductSeeder::class,
         ]);
     }
 }
